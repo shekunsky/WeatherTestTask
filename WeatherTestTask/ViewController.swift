79d18727cd16ad3772862f9699ea2e067ad60efb
Alex Shekunsky <shekunsky@gmail.com>
2019-04-08 13:27:58 +0300

    Initial Commit

---
+++ WeatherTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTestTask
+//
+//  Created by Alex2 on 4/8/19.
+//  Copyright © 2019 Alex Shekunsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
